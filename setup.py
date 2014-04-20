6b450d7b3b7bb5aeede964427d75984e7c48d5a9
Allen Li <darkfeline@abagofapples.com>
2014-04-20 01:50:18 -0400

    Update some stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='sadpanda-downloader',
+    version='0.1',
+    description='Sadpanda downloader script',
+    author='Allen Li',
+    author_email='darkfeline@abagofapples.com',
+    package_dir={'': 'src'},
+    scripts=['sadpanda']
+)
